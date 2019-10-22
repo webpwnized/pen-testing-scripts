566bc6c9d4e5ddbca76ab2cad6409844dfa41698
Jeremy Druin <webpwnized@users.noreply.github.com>
2019-10-22 16:27:28 -0400

    Rename parse-ports.sh to nmap/parse-ports.sh

--- parse-ports.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-GNMAP_FILE=$1
-
-egrep -v "^#|Status: Up" $GNMAP_FILE | cut -d' ' -f4- | sed -n -e 's/Ignored.*//p' | tr ',' '\n' | sed -e 's/^[ \t]*//' | sort -n | uniq | cut -d"/" -f1 | tr '\n' ',' | sed 's/,$//'
